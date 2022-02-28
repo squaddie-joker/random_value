bcd293a3c705b25598ad771bec8d4ba7ef0437c6
Squaddie Joker <squaddie-joker@yandex.ru>
2022-02-28 21:34:40 +1000

    Ecsperiments

---
+++ classes.py
@@ -0,0 +1,7 @@
+class User():
+    
+    
+    def __init__(self, user_name, user_email, user_password) -> None:
+        self.user_name = user_name
+        self.user_email = user_email
+        self.user_password = user_password
